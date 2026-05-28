10f45104c6a53207d7b0994969baee2aed024d38
Alexi Donckerwolcke <alexi.donckerwolcke@gmail.com>
2026-05-28 20:55:04 +0200

    Update script reference in index.html to use new asset version

--- client/src/lib/supabase.ts
+++ client/src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://supabase.co';
+const supabaseUrl = 'https://xuilgccacufwinvkocfl.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1aWxnY2NhY3Vmd2ludmtvY2ZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5NzY4OTYsImV4cCI6MjA4MTU1Mjg5Nn0.f0xa0hY6VDht7Qeqfbc0UaKpZLzCB43CXwOlfxDJ93M';
 
 if (!supabaseUrl || !supabaseAnonKey) {
