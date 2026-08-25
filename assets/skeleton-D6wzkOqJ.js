5576e710ccec3b594b72c69b54a7b989c10c525e
Alexi Donckerwolcke <your@email.com>
2026-08-25 12:49:24 +0200

    publish Chrome and Firefox extension downloads

---
+++ assets/skeleton-D6wzkOqJ.js
@@ -0,0 +1,1 @@
+import{j as m}from"./vendor-B5tHPFvP.js";import{g as o}from"./index-C-ediS-S.js";function n({className:e,...t}){return m.jsx("div",{className:o("animate-pulse rounded-md bg-muted",e),...t})}export{n as S};
