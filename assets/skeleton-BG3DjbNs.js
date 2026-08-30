a64bb97b3dc376db2db22ea1abe14a7fd0f5cd14
alexidonckerwolcke-byte <alexidonckerwolcke-byte@users.noreply.github.com>
2026-08-30 09:58:34 +0000

    deploy: 24f62696aeb058310571d83adbdacc621182dfd7

--- assets/skeleton-BG3DjbNs.js
+++
@@ -1,1 +0,0 @@
-import{j as m}from"./vendor-DSLiGKvi.js";import{g as o}from"./index-ZVrTkfUR.js";function n({className:e,...t}){return m.jsx("div",{className:o("animate-pulse rounded-md bg-muted",e),...t})}export{n as S};
