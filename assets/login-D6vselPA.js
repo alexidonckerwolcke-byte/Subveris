7f2417ac0def6da7a6b6d5bc20ed9f624554da28
Alexi Doncker Wolcke <alexidonckerwolcke@users.noreply.github.com>
2026-09-13 21:39:58 +0200

    Updates

---
+++ assets/login-D6vselPA.js
@@ -0,0 +1,1 @@
+import{z as n,j as a}from"./vendor-BxSZ0Bk3.js";import{A as i}from"./auth-modal-C4EIs2ll.js";import"./index-IWFiC-gq.js";import"./tabs-C81oNGYE.js";function u(){const[,o]=n();return a.jsx(i,{open:!0,defaultTab:"signin",onOpenChange:t=>{t||o("/")}})}export{u as default};
