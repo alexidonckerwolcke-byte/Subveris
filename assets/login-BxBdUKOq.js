d1ba085f456c26ba746ee3a89d427dcb37f6d929
Alexi Doncker Wolcke <alexidonckerwolcke@users.noreply.github.com>
2026-09-14 21:53:55 +0200

    Updates

---
+++ assets/login-BxBdUKOq.js
@@ -0,0 +1,1 @@
+import{z as n,j as a}from"./vendor-BxSZ0Bk3.js";import{A as i}from"./auth-modal-EDwFUge4.js";import"./index-Ufpm1L8J.js";import"./tabs-3bcdTPUf.js";function u(){const[,o]=n();return a.jsx(i,{open:!0,defaultTab:"signin",onOpenChange:t=>{t||o("/")}})}export{u as default};
