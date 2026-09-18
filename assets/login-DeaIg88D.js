09c3ad4f8f4a7c46f492bef032c40d83ac431337
alexidonckerwolcke-byte <alexidonckerwolcke-byte@users.noreply.github.com>
2026-09-18 21:28:43 +0000

    deploy: 2d538198ce812cd3552d1fa7e8d4062bbd676e54

---
+++ assets/login-DeaIg88D.js
@@ -0,0 +1,1 @@
+import{z as n,j as a}from"./vendor-BxSZ0Bk3.js";import{A as i}from"./auth-modal-CPysNyC_.js";import"./index-NEqgKSV-.js";import"./tabs-n5TdDlz-.js";function u(){const[,o]=n();return a.jsx(i,{open:!0,defaultTab:"signin",onOpenChange:t=>{t||o("/")}})}export{u as default};
