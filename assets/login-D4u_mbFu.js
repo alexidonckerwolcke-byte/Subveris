8b6bfb7c6701a1ae4f959353f8499d7732ee8d77
alexidonckerwolcke-byte <alexidonckerwolcke-byte@users.noreply.github.com>
2026-09-21 19:23:24 +0000

    deploy: 04e9b98c668a637a2a6a1ff453dca1543f258efa

---
+++ assets/login-D4u_mbFu.js
@@ -0,0 +1,1 @@
+import{z as n,j as a}from"./vendor-BxSZ0Bk3.js";import{A as i}from"./auth-modal-BEaseY7b.js";import"./index-BDH5xhMe.js";import"./tabs-B4Zk4hcc.js";function u(){const[,o]=n();return a.jsx(i,{open:!0,defaultTab:"signin",onOpenChange:t=>{t||o("/")}})}export{u as default};
